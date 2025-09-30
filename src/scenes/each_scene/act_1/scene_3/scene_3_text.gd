bd3375f5b5ddb9e74b1883d2d2e9fd42b113154e
Martín Saavedra López <160259195+MartinSaa@users.noreply.github.com>
2025-09-30 18:12:07 +0200

    update

--- src/scenes/each_scene/act_1/scene_3/scene_3_text.gd
+++ src/scenes/each_scene/act_1/scene_3/scene_3_text.gd
@@ -4,8 +4,8 @@ var text_dict: Dictionary = {
         0: {
             "text":["Espera, ¿Me pondré uno de los anillos?","Por qué no, ¿hoy siento la necesidad de llevarlos… pero… cuál?"],
             
-            "choices": ["Lo clásico siempre triunfa, vamos con el fino.","¿El grueso? Es más frio e incómodo, pero viendo como se ha puesto el día, parece que acompaña."],
-            "answers": [],
+            "choices": ["Coger anillo fino", "Coger anillo grueso"],
+            "answers": ["Lo clásico siempre triunfa, vamos con el fino.","¿El grueso? Es más frio e incómodo, pero viendo como se ha puesto el día, parece que acompaña."],
             "triggers": ["ring_1", "ring_2"],
             "actors": ["René"]
             },
